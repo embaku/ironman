9805e896bdde62ab004c5f1d8ef46fbafed8161a
embaku <118682078+embaku@users.noreply.github.com>
2022-11-21 15:53:20 +0530

    Update fract_knapsack.py

--- fract_knapsack.py
+++ fract_knapsack.py
@@ -1,33 +1,4 @@
-class Item:
 
-    def __init__(self,value,weight):
-        self.value=value
-        self.weight=weight
-
-
-def fractionaKnapsack(W,arr):
-    arr.sort(key=lambda x:(x.value/x.weight),reverse=True)
-    # N=len(arr)
-    # print(arr)
-    totalValue=0
-    for item in arr:
-
-        if item.weight<=W:
-            W-=item.weight
-            totalValue+=item.value
-        else:
-            totalValue=item.value*W/item.weight
-            break
-    return totalValue
-    
-
-if __name__=='__main__':
-
-    W=50
-    arr=[Item(20,5),Item(60,20),Item(100,40),Item(50,10)]
-
-    ans=fractionaKnapsack(W,arr)
-    print(f"Max possible profit: {ans}")
 
 class Item:
     def __init__(self, value, weight):
